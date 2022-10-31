6c64b99470dcd4ea7d527e5d606e9e2a0feefe77
RaeesaParker <raeesaparker101@gmail.com>
2022-10-31 19:29:16 +0000

    Added route changing to menu bar

--- src/components/header.js
+++ src/components/header.js
@@ -4,6 +4,9 @@ import React from 'react';
 function Heading(props){
 
 
+  function contentsPageFunc(){
+    props.setRoute('menu')
+  }
 
   // ----------------------------------------------------------------- //
   // Define the return
@@ -14,7 +17,7 @@ function Heading(props){
       <nav className="navbar navbar-expand-lg  navbar-customise">
         <div className="navbar-container">
           <a className="navbar-brand" href="#">Wizarding Spell Book</a>
-          <a>Contents</a>
+          <a onClick={()=> contentsPageFunc()}>Contents</a>
         </div>
       </nav>
     </div>
