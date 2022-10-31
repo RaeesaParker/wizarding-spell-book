151eeca76bde7e8c342c34917708d1c7bbf59906
RaeesaParker <raeesaparker101@gmail.com>
2022-10-31 15:21:41 +0000

    Added one spell card

--- src/components/functionality.js
+++ src/components/functionality.js
@@ -3,56 +3,68 @@ import { useState, useEffect } from 'react';
 import SpellCard from './subcomponents/spellCard';
 
 
-
 function Functionality(){
 
+  // Get the data from the api - https://wizard-world-api.herokuapp.com/Spells?Type=Charm
+  // Map through the array 
+  // Create a card for each spell
 
-    // Get the data from the api - https://wizard-world-api.herokuapp.com/Spells?Type=Charm
-    // Map through the array 
-    // Create a card for each spell
+  // Set state to store the spells
+  const [spells, setSpells] = useState([]);
 
-    // Set state to store the spells
-    const [spells, setSpells] = useState([]);
+  // Set state to detect change & trigger api
+  const [change, setChange] = useState(0);
 
-    // Set state to detect change & trigger api
-    const [change, setChange] = useState(0);
+  // Set state to store any errors
+  const [error, setError] = useState(null)
 
-    // Get the data 
-    useEffect(() => {
-        async function getSpells(){
-            try{
-                const response = await fetch(
-                 " https://wizard-world-api.herokuapp.com/Spells?Type=Charm"
-                );
-            
-                // Error if there response is not okay
-                if (!response.ok){
-                    throw new Error(response.statusText) 
-                }
-            
-                // Set the state of the spells
-                const dataSpells = await response.json();
-                setSpells(dataSpells);
-            } 
+
+  // Get the data 
+  useEffect(() => {
+    async function getSpells(){
+      try{
+        const response = await fetch(
+        " https://wizard-world-api.herokuapp.com/Spells?Type=Charm"
+        );
           
-            //  Catch any errors
-            catch (err){
-                setError("Could not fetch data");
-                console.log(err.message)
-            }
-        };
+        // Error if there response is not okay
+        if (!response.ok){
+          throw new Error(response.statusText) 
+        }
+          
+        // Set the state of the spells
+        const dataSpells = await response.json();
+        setSpells(dataSpells);
+        console.log(spells[0])
+      } 
         
-        getSpells();
-      }, [change]);
+      // Catch any errors
+      catch (err){
+        setError("Could not fetch data");
+        console.log(err.message)
+      }
+    };    
+    // Get the spells when a change has been detected
+    getSpells();
+  }, [change]);
+
+
+  // Function to make a change => Triggers the useEffect
+  const addOne = () => {
+    setChange(change + 1);
+  };
   
-  
-    return (
-      <div>
-        <h2>Functionality</h2>
+
+  return (
+    <div>
+      <div className='spell-container'>
+        <SpellCard name={spells[0].name} effect={spells[0].effect} incantation={spells[0].incantation} light={spells[0].light} > </SpellCard>
       </div>
-      )
-  
-  }
-  
-  
-  export default Functionality;
+      <button onClick={addOne}>Generate Spells</button>
+    </div>
+    )
+
+}
+
+
+export default Functionality;
--- src/components/subcomponents/spellCard.js
+++ src/components/subcomponents/spellCard.js
@@ -2,14 +2,21 @@ import React from 'react';
 
 
 
-function SpellCard(){
+function SpellCard(props){
 
 
   
   
     return (
-      <div>
-        <h2>SpellCard</h2>
+      <div className='spell-card'>
+        <h3>{props.name}</h3>
+        <h4>Effect</h4>
+        <p>{props.effect}</p>
+        <h4>Incantation</h4>
+        <p>{props.incantation}</p>
+        <h4>Light</h4>
+        <p>{props.light}</p>
+
       </div>
       )
   
