f2e1c49085e213e3d0d80b3e04a4f3be2bfb0032
RaeesaParker <raeesaparker101@gmail.com>
2022-10-31 13:55:55 +0000

    Added variables

--- src/components/functionality.js
+++ src/components/functionality.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
 import SpellCard from './subcomponents/spellCard';
 
 
@@ -6,6 +7,43 @@ import SpellCard from './subcomponents/spellCard';
 function Functionality(){
 
 
+    // Get the data from the api - https://wizard-world-api.herokuapp.com/Spells?Type=Charm
+    // Map through the array 
+    // Create a card for each spell
+
+    // Set state to store the spells
+    const [spells, setSpells] = useState([]);
+
+    // Set state to detect change & trigger api
+    const [change, setChange] = useState(0);
+
+    // Get the data 
+    useEffect(() => {
+        async function getSpells(){
+            try{
+                const response = await fetch(
+                 " https://wizard-world-api.herokuapp.com/Spells?Type=Charm"
+                );
+            
+                // Error if there response is not okay
+                if (!response.ok){
+                    throw new Error(response.statusText) 
+                }
+            
+                // Set the state of the spells
+                const dataSpells = await response.json();
+                setSpells(dataSpells);
+            } 
+          
+            //  Catch any errors
+            catch (err){
+                setError("Could not fetch data");
+                console.log(err.message)
+            }
+        };
+        
+        getSpells();
+      }, [change]);
   
   
     return (
