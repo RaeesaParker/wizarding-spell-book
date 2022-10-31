d063cb32b189eb11c9c9919b9f5117684ba6e6fc
RaeesaParker <raeesaparker101@gmail.com>
2022-10-31 15:26:59 +0000

    Added modal package

--- src/components/subcomponents/spellCard.js
+++ src/components/subcomponents/spellCard.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Modal from 'react-modal';
 import SpellCardModal from './spellCardModal';
 
 
@@ -7,13 +8,6 @@ function SpellCard(props){
     return (
       <div className='spell-card'>
         <h3>{props.name}</h3>
-        <h4>Effect</h4>
-        <p>{props.effect}</p>
-        <h4>Incantation</h4>
-        <p>{props.incantation}</p>
-        <h4>Light</h4>
-        <p>{props.light}</p>
-
       </div>
       )
   
