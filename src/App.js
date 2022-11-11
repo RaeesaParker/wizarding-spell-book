71c428080705048270713f3766f42b929df42de4
RaeesaParker <raeesaparker101@gmail.com>
2022-11-11 15:06:28 +0000

    Add routing fix

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { useState } from 'react';
 import { BrowserRouter , Routes , Route , Link   } from 'react-router-dom';
 import Heading from './components/Header';
 import Functionality from './components/Functionality';
+import About from './components/About';
 import Footer from './components/Footer';
 
 function App() {
@@ -12,7 +13,7 @@ function App() {
       <BrowserRouter>
       <Heading></Heading>
       {/* <Routes path='wizarding-spell-book/'
-          element={<Functionality></Functionality>} >
+          element={<About></About>} >
       </Routes> */}
       <Functionality></Functionality>
         <Footer></Footer>
