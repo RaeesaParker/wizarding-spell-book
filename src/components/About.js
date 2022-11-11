35f4ccc8538d5954d67b2f4077a785ae8090a436
RaeesaParker <raeesaparker101@gmail.com>
2022-11-11 10:15:27 +0000

    Add homepagee

--- src/components/About.js
+++ src/components/About.js
@@ -3,11 +3,12 @@ import React from 'react';
 function About(){
   
   return (
-    <div className='section-menu'>
-      
-      <h2>Welcome to your Spell Book!</h2>
-
-    </div>
+      <div className='section-about '>
+        <h2>Welcome to your Spell Book!</h2>
+        <p className='section-about-para'>This book contains all the spells you will ever need on your wizarding journey! </p>
+        <p>Begin by choosing a spell catergory on the contents page.</p>
+        <p></p>
+      </div>
   )
 
 }
